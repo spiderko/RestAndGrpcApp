fdfce4177da6364e1181135828c30b5411fce39c
spiderko <krzysztof.pajak.leamington@gmail.com>
2025-01-26 15:20:43 +0000

    Update EngineService.cs

--- RestAndGrpcApp/Services/EngineService.cs
+++ RestAndGrpcApp/Services/EngineService.cs
@@ -3,7 +3,7 @@ using RestAndGrpcApp.Protos;
 
 namespace RestAndGrpcApp.Server.Services {
   public class EngineServiceImpl() : EngineService.EngineServiceBase {
-    public override JobCreateResponse JobCreate(JobCreateRequest) {
+    public override Task<JobCreateResponse> JobCreate(JobCreateRequest request, ServerCallContext context) {
     
     }
   }
